f0d9089f28c3a75861449c01802411d9057480f8
ofrich14 <ofrich.home@gmail.com>
2024-05-12 18:52:36 +0300

    tikun bagim bemanoim

--- main.ts
+++ main.ts
@@ -1,24 +1,56 @@
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "l") {
-        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M2,
+        255,
+        SuperBit.enMotors.M4,
+        0
+        )
         basic.pause(1000)
-        SuperBit.MotorRun(SuperBit.enMotors.M4, 0)
-        basic.pause(1000)
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # # # .
+            `)
+        basic.clearScreen()
     } else if (receivedString == "r") {
         SuperBit.MotorRun(SuperBit.enMotors.M4, 255)
-        basic.pause(1000)
         SuperBit.MotorRun(SuperBit.enMotors.M2, 0)
         basic.pause(1000)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # # . .
+            . # . . .
+            . # . . .
+            `)
+        basic.clearScreen()
     } else if (receivedString == "f") {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        basic.pause(3000)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        basic.pause(3000)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+        SuperBit.MotorRun(SuperBit.enMotors.M4, 255)
+        basic.pause(1000)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . # . . .
+            . # . . .
+            `)
+        basic.clearScreen()
     } else if (receivedString == "b") {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
+        SuperBit.MotorRun(SuperBit.enMotors.M4, -255)
         basic.pause(3000)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-        basic.pause(3000)
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            `)
+        basic.clearScreen()
     } else {
         basic.showLeds(`
             # . . . #
